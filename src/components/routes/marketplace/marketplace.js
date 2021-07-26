bc04629d7dd4ceecbd9ca4f12f7c7f99d76de8df
loque <madridx2010@gmail.com>
2021-07-25 20:47:22 -0500

    marketplace pagination

--- src/components/routes/marketplace/marketplace.js
+++ src/components/routes/marketplace/marketplace.js
@@ -4,25 +4,91 @@ import NftCard from '../../cards/nftcard';
 import splitEvery from '../../utils/columnsSplitter';
 import axios from 'axios';
 
+import './marketplace.scss';
+
 const Marketplace = props => {
 
     const [nfts, setNfts] = useState([]);
+
+    const [current, setCurrent] = useState(20);
 
     useEffect(
         () => {
 
             axios.get('https://jsonplaceholder.typicode.com/todos')
-            .then(res => setNfts(res.data) );
+            .then(res => setNfts(res.data.splice(0, current)) );
 
         },
         []
     );
 
+    const onNextClicked = e => {
+        e.preventDefault();
+
+        setNfts([]);
+
+        const iniVal = current;
+
+        setCurrent(current + current);
+
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(res => setNfts(res.data.splice(iniVal, current)) );
+    }
+
+
 
     return(
         <div>
+
+            {/* artwork */}
             <section className="block has-text-centered p-6 has-background-dark mb-0">
                 <div className="container">
+
+                    <figure className="image is-3by1 banner">
+                        <img src="https://i.pinimg.com/originals/4e/a8/4f/4ea84f04d08d72d2cd574d14f5dad176.jpg"/>
+                    </figure>
+
+
+
+
+
+                </div>
+            </section>
+
+
+
+            <section className="block has-text-centered p-6 has-background-dark mb-0">
+                <div className="container">
+
+                <nav class="pagination" role="navigation" aria-label="pagination">
+                  <a class="pagination-previous has-text-white">Previous</a>
+                  <a class="pagination-next has-text-white" onClick={onNextClicked}>Next page</a>
+                  <ul class="pagination-list">
+                    <li>
+                      <a class="pagination-link has-text-white" onClick={onNextClicked} aria-label="Goto page 1">1</a>
+                    </li>
+                    <li>
+                      <span class="pagination-ellipsis">&hellip;</span>
+                    </li>
+                    <li>
+                      <a class="pagination-link has-text-white" aria-label="Goto page 45">45</a>
+                    </li>
+                    <li>
+                      <a class="pagination-link has-text-white is-current" aria-label="Page 46" aria-current="page">46</a>
+                    </li>
+                    <li>
+                      <a class="pagination-link has-text-white" aria-label="Goto page 47">47</a>
+                    </li>
+                    <li>
+                      <span class="pagination-ellipsis">&hellip;</span>
+                    </li>
+                    <li>
+                      <a class="pagination-link has-text-white" aria-label="Goto page 86">86</a>
+                    </li>
+                  </ul>
+                </nav>
+
+
 
                     {/* Search bar & filters */}
                     <div className="columns">
