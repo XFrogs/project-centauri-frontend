e0f185d1c0887a645a0c446f6b2c58c68fb23449
loque <madridx2010@gmail.com>
2021-08-14 00:36:50 -0500

    fix try #3

--- src/components/routes/home/home.js
+++ src/components/routes/home/home.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import {baseURL} from '../../../images.js';
-import "./home.scss";
+import "./home.css";
 import {Link} from 'react-router-dom';
 import NftCard from '../../cards/nftcard';
 
